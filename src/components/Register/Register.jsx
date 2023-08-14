3cd8243c58a0908ebb056130d680e5f6f11423c9
Alexey Smirnov <alexey.smirnov.barnaul@yandex.ru>
2023-08-14 14:49:35 +0700

    add error display on register form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -8,7 +8,7 @@ function Register() {
   return (
     <section className='reg'>
       <Link to='/' className='reg__logo'>
-        <img src={logo} alt='Логотип' />
+        <img src={logo} alt='Логотип' className='reg__logo-image' />
       </Link>
       <h1 className='reg__title'>Добро пожаловать!</h1>
       <form className='reg__form'>
@@ -23,7 +23,13 @@ function Register() {
         <label className='reg__label' htmlFor='password'>
           Пароль
         </label>
-        <input className='reg__input' type='password' required id='password' />
+        <input
+          className='reg__input reg__input_type_error'
+          type='password'
+          required
+          id='password'
+        />
+        <span className='reg__error-massage'>Что-то пошло не так...</span>
         <button className='reg__button'>Зарегистрироваться</button>
       </form>
 
