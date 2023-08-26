df5cf5c510cd397f2816655b11feeeb7ea042d19
Alexey Smirnov <alexey.smirnov.barnaul@yandex.ru>
2023-08-26 17:26:17 +0700

    change AboutMe text and photo

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -10,14 +10,11 @@ function AboutMe() {
       <SectionHeader text='Студент' padding_top='110px' />
       <div className='about-me__container'>
         <div className='about-me__text'>
-          <h3 className='about-me__name'>Виталий</h3>
-          <p className='about-me__profession'>Фронтенд-разработчик, 30 лет</p>
+          <h3 className='about-me__name'>Алексей</h3>
+          <p className='about-me__profession'>Фронтенд-разработчик</p>
           <p className='about-me__description'>
-            Я родился и живу в Саратове, закончил факультет экономики СГУ. У
-            меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь
-            бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ
-            Контур». После того, как прошёл курс по веб-разработке, начал
-            заниматься фриланс-заказами и ушёл с постоянной работы.
+            Я родился и живу в Барнауле, закончил с отличием бакалавриат
+            Прикладная Информатика АлтГУ.
           </p>
           <a
             className='about-me__link'
